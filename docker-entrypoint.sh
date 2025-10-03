0c3e69a1946d184c5508137c971a9906e57982d3
Lucas Nogueira <lucasttn@MacBook-Pro-de-Lucas.local>
2025-10-03 20:14:48 -0300

    refactor: simplify deployment by removing Docker and using serve for static files

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Define PORT como 80 se não estiver definida (fallback para ambiente local)
-export PORT=${PORT:-80}
-
-# Executa o entrypoint padrão do nginx
-exec /docker-entrypoint.sh "$@"
-
